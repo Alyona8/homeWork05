413040751825b616e8e21ff32b6d65030201f74a
Alyona <alyonushka489@gmail.com>
2022-08-12 21:17:15 +0300

    Исправленное ДЗ 5

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
         //Задача 1
-        int[] array = new int[3];
-        array[0] = 1;
-        array[1] = 2;
-        array[2] = 3;
+        int[] array = new int[]{1, 2, 3};
 
         double[] arrayFrac = {1.57, 7.654, 9.986};
 
@@ -13,74 +12,45 @@ public class Main {
 
 
         //Задача 2
-        for (int i = 0; i <= 2; i++) {
-            if (i == 2) {
-                System.out.print(array[i]);
-            } else {
-                System.out.print(array[i] + ", ");
-            }
+        for (int i = 0; i < array.length - 1; i++) {
+        System.out.print(array[i] + ", ");
         }
+        System.out.println(array[array.length - 1]);
 
-        System.out.print("\n");
-
-        for (int i = 0; i <= 2; i++) {
-            if (i == 2) {
-                System.out.print(arrayFrac[i]);
-            } else {
-                System.out.print(arrayFrac[i] + ", ");
-            }
+        for (int i = 0; i < arrayFrac.length - 1; i++) {
+        System.out.print(arrayFrac[i] + ", ");
         }
+        System.out.println(arrayFrac[arrayFrac.length - 1]);
 
-        System.out.print("\n");
-
-        for (int i = 0; i <= 2; i++) {
-            if (i == 2) {
-                System.out.print(arbitrary[i]);
-            } else {
-                System.out.print(arbitrary[i] + ", ");
-            }
+        for (int i = 0; i < arbitrary.length - 1; i++) {
+        System.out.print(arbitrary[i] + ", ");
         }
-
-        System.out.print("\n");
+        System.out.println(arbitrary[arbitrary.length - 1]);
 
 
         //Задача 3
-        for (int i = 2; i >= 0; i--) {
-            if (i == 0) {
-                System.out.print(array[i]);
-            } else {
-                System.out.print(array[i] + ", ");
-            }
+        for (int i = array.length - 1; i > 0; i--) {
+        System.out.print(array[i] + ", ");
         }
+        System.out.println(array[0]);
 
-        System.out.print("\n");
-
-        for (int i = 2; i >= 0; i--) {
-            if (i == 0) {
-                System.out.print(arrayFrac[i]);
-            } else {
-                System.out.print(arrayFrac[i] + ", ");
-            }
+        for (int i = arrayFrac.length - 1; i > 0; i--) {
+            System.out.print(arrayFrac[i] + ", ");
         }
+        System.out.println(arrayFrac[0]);
 
-        System.out.print("\n");
-
-        for (int i = 2; i >= 0; i--) {
-            if (i == 0) {
-                System.out.print(arbitrary[i]);
-            } else {
-                System.out.print(arbitrary[i] + ", ");
-            }
+        for (int i = arbitrary.length - 1; i > 0; i--) {
+            System.out.print(arbitrary[i] + ", ");
         }
-
-        System.out.print("\n");
+        System.out.println(arbitrary[0]);
 
 
         //Задача 4
         for(int i = 0; i < array.length; i++) {
-            if (array[i] % 2 != 0)
+            if (array[i] % 2 != 0) {
                 array[i] = array[i] + 1;
-            System.out.print(array[i] + " ");
+            }
+        }
+            System.out.println(Arrays.toString(array));
         }
     }
-}
